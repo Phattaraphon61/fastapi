394e655e81233f5a575ad608d4c1e2acdcc52e69
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-28 18:00:07 +0700

    test07

--- main.py
+++ main.py
@@ -8,13 +8,3 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message":"Hello TutLinks.com"}
-from fastapi import FastAPI
-
-app = FastAPI()
-
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
-
-@app.get("/")
-def home():
-    return {"message":"Hello TutLinks.com"}
