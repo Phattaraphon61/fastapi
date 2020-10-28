7f159cca9da582e97bd35947276c01c5c59777d7
Phattaraphon Pewruang <phattaraphon.pe.61@ubu.ac.th>
2020-10-28 18:10:42 +0700

    test10

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message":"Hello TutLinks.com"}
+@app.get("/b2s/{text}")
+def bit2int(text:str):
+    s = int(text[0])
+    e = int(text[1:9], 2)
+    f = [ int(x) for x in text[9:]]
+    x = 1 + sum([ int(f[i])*2**(-(i+1)) for i in range(len(f)) ])
+    result = (-1)**s * 2**(e-127) * x 
+    return result
